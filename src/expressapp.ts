6a2bc464ab3504ede101a3ae13e7310c7f427681
wzr1337 <bartsch98@gmail.com>
2017-02-08 23:02:24 +0100

    chore(expressapp): reindent 2 spaces

--- src/expressapp.ts
+++ src/expressapp.ts
@@ -7,108 +7,108 @@ import { viwiLogger, viwiLoggerInstance } from "./log";
 
 
 class viwiWebSocket {
-    constructor(private ws: WebSocket) {
+  constructor(private ws: WebSocket) {
+  }
+  
+  data(event:string, payload:Object):void {
+    this.ws.send(JSON.stringify({type: "data", status: "ok", event: event, data: payload}));
+  }
+  
+  error(code:number, err:Error):void {
+    this.ws.send(JSON.stringify({type: "error", code: code, data: err.message}));
+  }
+  
+  subscribeAck(event:string):void {
+    this.ws.send(JSON.stringify({type: "subscribe", status: "ok", event: event}));
+  }
+  
+  unsubscribeAck(event:string):void {
+    this.ws.send(JSON.stringify({type: "unsubscribe", status: "ok", event: event}));
+  }
+}
+
+
+
+// create server and listen on provided port (on all network interfaces).
+class WebServer {
+  public app: express.Express;
+  public ws: WebSocketServer.Server;
+  private _server:any;
+  private _port:number|string|boolean;
+  private _logger:viwiLoggerInstance;
+  
+  constructor (_port?:number) {
+    this._logger = viwiLogger.getInstance().getLogger("general");
+    this.app = express();
+    
+    var whitelist = ['127.0.0.1'];
+    let corsOpts:cors.CorsOptions = {
+      origin: function (origin, callback) {
+        let originIsWhitelisted = whitelist.indexOf(origin) !== -1
+        callback(originIsWhitelisted ? null : new Error('Bad Request'), originIsWhitelisted)
+      },
+      exposedHeaders: 'Location'
     }
-
-    data(event:string, payload:Object):void {
-        this.ws.send(JSON.stringify({type: "data", status: "ok", event: event, data: payload}));
-    }
-
-    error(code:number, err:Error):void {
-        this.ws.send(JSON.stringify({type: "error", code: code, data: err.message}));
-    }
-
-    subscribeAck(event:string):void {
-        this.ws.send(JSON.stringify({type: "subscribe", status: "ok", event: event}));
-    }
-
-    unsubscribeAck(event:string):void {
-        this.ws.send(JSON.stringify({type: "unsubscribe", status: "ok", event: event}));
-    }
-    }
-
-
-
-    // create server and listen on provided port (on all network interfaces).
-    class WebServer {
-    public app: express.Express;
-    public ws: WebSocketServer.Server;
-    private _server:any;
-    private _port:number|string|boolean;
-    private _logger:viwiLoggerInstance;
-
-    constructor (_port?:number) {
-        this._logger = viwiLogger.getInstance().getLogger("general");
-        this.app = express();
-
-        var whitelist = ['127.0.0.1'];
-        let corsOpts:cors.CorsOptions = {
-            origin: function (origin, callback) {
-                let originIsWhitelisted = whitelist.indexOf(origin) !== -1
-                callback(originIsWhitelisted ? null : new Error('Bad Request'), originIsWhitelisted)
-            },
-            exposedHeaders: 'Location'
-        }
-
-        this.app.use(cors());
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-        this.app.use((req:express.Request, res:express.Response, next:express.NextFunction) => {
-            this._logger.info("Query:", req.method, req.url);
-            next();
-        })
-
-        // Get port from environment and store in Express.
-        this._port = this.normalizePort(process.env.PORT || _port || '3000');
-        this.app.set('port', this._port);
-
-        this._server = http.createServer(this.app);
-
-        this.ws = new WebSocketServer.Server({ server: this._server });
-    }
-
-    init() {
-        this._server.listen(this._port);
-        this._server.on('listening', this.onListening);
-    }
-
-    /**
-     * Normalize a port into a number, string, or false.
-     */
-    normalizePort(val: any): number|string|boolean {
+    
+    this.app.use(cors());
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use((req:express.Request, res:express.Response, next:express.NextFunction) => {
+      this._logger.info("Query:", req.method, req.url);
+      next();
+    })
+    
+    // Get port from environment and store in Express.
+    this._port = this.normalizePort(process.env.PORT || _port || '3000');
+    this.app.set('port', this._port);
+    
+    this._server = http.createServer(this.app);
+    
+    this.ws = new WebSocketServer.Server({ server: this._server });
+  }
+  
+  init() {
+    this._server.listen(this._port);
+    this._server.on('listening', this.onListening);
+  }
+  
+  /**
+  * Normalize a port into a number, string, or false.
+  */
+  normalizePort(val: any): number|string|boolean {
     let port = parseInt(val, 10);
-
+    
     if (isNaN(port)) {
-        // named pipe
-        return val;
+      // named pipe
+      return val;
     }
-
+    
     if (port >= 0) {
-        // port number
-        return port;
+      // port number
+      return port;
     }
-
+    
     return false;
-    }
-
-    /**
-     * Event listener for HTTP server "listening" event.
-     */
-    onListening = () => {
+  }
+  
+  /**
+  * Event listener for HTTP server "listening" event.
+  */
+  onListening = () => {
     let addr = this._server.address();
     let bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+    
     console.log('Listening on ' + bind);
-    }
-
-    /**
-     * Shutdown the server
-     */
-    close = () => {
+  }
+  
+  /**
+  * Shutdown the server
+  */
+  close = () => {
     this._server.close();
-    }
+  }
 };
 
 export { WebServer, viwiWebSocket };
