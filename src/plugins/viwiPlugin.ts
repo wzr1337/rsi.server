e9c87a6cc55a4eb16d68cfc4f6634b94c0017860
wzr1337 <bartsch98@gmail.com>
2017-03-10 21:08:15 +0100

    refactor(viwiPlugin): let viwiPlugin be a first class citizen

---
+++ src/plugins/viwiPlugin.ts
@@ -0,0 +1,44 @@
+import { BehaviorSubject, Subject } from '@reactivex/rxjs';
+
+export enum Status {
+  "OK" = 200,
+  "CREATED" = 201,
+  "ACCEPTED" = 202,
+  "MALFORMED_REQUEST" = 400,
+  "FORBIDDEN" = 403,
+  "NOT_FOUND" = 404,
+  "INTERNAL_SERVER_ERROR" = 500
+}
+
+export interface Service {
+  name:string;
+  id: string;
+  resources:Resource[];
+}
+
+export interface Element {
+  lastUpdate: number;
+  propertiesChanged: string[];
+  data: any;
+}
+
+export interface ResourceUpdate {
+  lastUpdate: number;
+  action: "init"|"add"|"move"|"remove";
+}
+
+export interface Resource {
+  name:string;
+  change:Subject<ResourceUpdate>;
+
+  //@TODO: return a promise and handle success/failure accordingly
+  getResource?(offset?:string|number, limit?:string|number):BehaviorSubject<Element>[]; //GET /<service>/<resource>/
+  createElement?(state:{}):Element|Status;                                          //POST /<service>/<resource>/
+  //@TODO: return a promise and handle success/failure accordingly
+  getElement(elementId:string):BehaviorSubject<Element>;                                //GET /<service>/<resource>/<element>
+  updateElement?(elementId:string, difference:any):Boolean;                             //POST /<service>/<resource>/<element>
+  deleteElement?(elementId:string):Boolean;                                             //DELETE /<service>/<resource>/<element>
+
+  resourceSubscribable?:Boolean;    //subscribe /<service>/<resource>/
+  elementSubscribable?:Boolean;     //subscribe /<service>/<resource>/<element>
+}
