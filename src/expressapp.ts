bd0c311c4fe87fb0f6b3aa24754017f6ee164390
wzr1337 <bartsch98@gmail.com>
2017-08-08 18:23:50 +0200

    fix(cdn): register under cdn instead of cdn2

--- src/expressapp.ts
+++ src/expressapp.ts
@@ -55,10 +55,7 @@ class WebServer {
     this.app.use(compression());
 
     //serve static content for cdn
-    this.app.use('/cdn/images', express.static(path.join(__dirname, 'cdn', 'images')));
-
-
-    this.app.use('/cdn2/images', Cdn.getInstance().process());
+    this.app.use('/cdn/images', Cdn.getInstance().process());
 
     // Get port from environment and store in Express.
     this._port = this.normalizePort(process.env.PORT || _port || '3000');
